f8f086e840409b2080d47907feaaa614fb7c4e2f
Damian Mooyman <damian@silverstripe.com>
2014-08-11 16:18:30 +1200

    ueoueoaueu

--- travis_setup_selenium.php
+++ travis_setup_selenium.php
@@ -28,8 +28,8 @@ if(!$resolution) {
 echo "Starting xvfb at {$resolution}...\n";
 $XVFBARGS = ":99 -ac -screen 0 {$resolution}x24";
 putenv("XVFBARGS={$XVFBARGS}");
-run("/usr/bin/Xvfb $XVFBARGS");
-run("/sbin/start-stop-daemon --start --quiet --pidfile /tmp/cucumber_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- $XVFBARGS");
+run("/usr/bin/Xvfb $XVFBARGS &");
+//run("/sbin/start-stop-daemon --start --quiet --pidfile /tmp/cucumber_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- $XVFBARGS");
 
 if(!putenv("DISPLAY=:99")) echo "ERROR: Could not set display!\n";
 run("wget https://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar");
